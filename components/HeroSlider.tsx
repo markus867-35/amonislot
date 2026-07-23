593a95a490eb197b03b194e6c4ec2e9eb0916e62
Markus Erwin Ndruru <markussuperhoki@gmail.com>
2026-07-23 11:55:59 +0700

    update css

--- components/HeroSlider.tsx
+++ components/HeroSlider.tsx
@@ -1,6 +1,6 @@
 export default function HeroSlider() {
   return (
-    <div className="w-full h-[300px] md:h-[400px] bg-gray-800 flex items-center justify-center overflow-hidden border-b border-purple-800 relative">
+    <div className="w-full h-[px] md:h-[500px] bg-gray-800 flex items-center justify-center overflow-hidden border-b border-purple-800 relative">
       <img 
         src="https://ik.imagekit.io/j72i7hsy1/BANNER1.PNG" 
         alt="Hero Banner AMONISLOT" 
